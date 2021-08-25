6988d7ed6b72610e046b8584ac03b6003cf7ad02
sch3fr <sch3fr@gmail.com>
2021-08-25 18:07:16 +0200

    velocity and acceleration calculations now working

--- consoleMAPC/consoleMAPC/Program.cs
+++ consoleMAPC/consoleMAPC/Program.cs
@@ -4,15 +4,11 @@ namespace consoleMAPC
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//TODO převodník jednotek
         {
             bool quitSwitch = false;
             char choice;
-            string menu = "\n1. Mechanika\n2. Molekulová fyzika a termika\n3. Mechanické kmitání a vlnění\n4. Elektřina a magnetismus\n5. Optika\n6. Teorie relativity\n7. Fyzika mikrosvěta\n\nZmáčkněte 0 pro ukončení programu.";
-
-            Console.WriteLine("Milošova kvalitní fyzikální kalkulačka <3 v 0.1");
-            Console.WriteLine("\nVyberte si obor, z něhož je váš příklad.");
-            Console.WriteLine(menu);
+            Menu();
 
             do
             {
@@ -53,10 +49,90 @@ namespace consoleMAPC
 
 
         }
-        public static void Mechanika()
+        public static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("Mechanika");
+            Console.WriteLine("Milošova kvalitní fyzikální kalkulačka <3 v 0.1");
+            Console.WriteLine("\nVyberte si obor, z něhož je váš příklad.");
+            Console.WriteLine("\n1. Mechanika\n2. Molekulová fyzika a termika\n3. Mechanické kmitání a vlnění\n4. Elektřina a magnetismus\n5. Optika\n6. Teorie relativity\n7. Fyzika mikrosvěta\n\nZmáčkněte 0 pro ukončení programu.");
+        }
+        public static void Mechanika()
+        {
+            bool localSwitch = false;
+            char localChoice;
+            int easterEgg = 0;
+            Console.Clear();
+            Console.WriteLine("Mechanika\n\nCo počítáme?\nPro výběr zmáčkni klávesu před veličinou (a-j)\n");
+            Console.WriteLine("a. rychlost\nb. zrychlení\nc. čas\nd. dráhu\ne. hybnost\nf. sílu\ng. výkon\nh. rychlost vody\ni. tlak\nj. hustota");
+
+            do 
+            {
+                localChoice = char.ToLower(Console.ReadKey().KeyChar);
+                switch (localChoice)
+                {
+                    case 'a'://rychlost
+                        Console.WriteLine("\nZadejte dráhu v m");
+                        double draha = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Zadejte čas v s");
+                        double cas = Convert.ToDouble(Console.ReadLine());
+                        double vysledenaRychlost = draha / cas;
+                        Console.WriteLine("\nVýsledek je {0} m.s^-1", vysledenaRychlost);
+                        Console.WriteLine("Stiskem jakéhokoliv tlačítka se vrátíte hlavního do menu");
+                        Console.ReadKey();
+                        localSwitch = true;
+                        break;
+                    case 'b'://zrychlení
+                        Console.WriteLine("\nZadejte změnu rychlosti v m.s^-1");
+                        double rychlostDelta = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Zadejte čas v s");
+                        double casDelta = Convert.ToDouble(Console.ReadLine());
+                        double vysledneZrychleni = rychlostDelta / casDelta;
+                        Console.WriteLine("\nVýsledek je {0} m.s^-2", vysledneZrychleni);
+                        Console.WriteLine("Stiskem jakéhokoliv tlačítka se vrátíte hlavního do menu");
+                        Console.ReadKey();
+                        localSwitch = true;
+                        break;
+                    case 'c'://čas
+                        Console.WriteLine("\nPočítáme dobu vzhledem k\na. rychlosti, nebo \nb. ke zrychlení?");
+                        
+                        localSwitch = true;
+                        break;
+                    case 'd'://dráha
+                        //do
+                        break;
+                    case 'e'://hybnost
+                        //do
+                        break;
+                    case 'f'://síla
+                        //do
+                        break;
+                    case 'g'://výkon
+                        //do
+                        break;
+                    case 'h'://rychlost vody
+                        //do
+                        break;
+                    case 'i'://tlak
+                        //do
+                        break;
+                    case 'j'://hustota
+                        //do
+                        break;
+                    default:
+                        if (easterEgg < 8)
+                        {
+                            Console.WriteLine("\nMusíš vybrat jedno z výše uvedených písmen");
+                            easterEgg++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nHele mě už to nebaví. Jestli budeš dělat kraviny tak se ukončím >:(");
+                        }
+                        break;
+                }
+            }
+            while (localSwitch == false);
+            Menu();
         }
         public static void MoleTermik()
         {
